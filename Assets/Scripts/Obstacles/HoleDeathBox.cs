9ce47a310b3fbbbfee278a08b9e4d89172f19b06
Kyldros <kyldros@gmail.com>
2024-01-25 15:20:40 +0100

    Update HoleDeathBox.cs

--- Assets/Scripts/Obstacles/HoleDeathBox.cs
+++ Assets/Scripts/Obstacles/HoleDeathBox.cs
@@ -6,6 +6,10 @@ public class HoleDeathBox : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        
+        Player player = other.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            player.takeDamage(damage);
+        }
     }
 }
