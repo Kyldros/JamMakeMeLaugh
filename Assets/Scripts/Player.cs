6f3da2ec35628f319f8df4a9f408c908ca375a79
xGiordan23x <francigio23@gmail.com>
2024-01-25 15:18:50 +0100

    cose

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,36 +1,68 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float speed;
-   
-    private bool isRagdoll;
     
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float ragdollSpeed;
+    public Rigidbody rb;
+    public List<Rigidbody> ragdollRb;
 
-    
-    public    void OnEnable()
+    private bool isRagdoll = false;
+    private Animator anim;
+
+
+
+    public void OnEnable()
     {
-       
+        ragdollRb = GetComponentsInChildren<Rigidbody>().ToList();
+        ragdollRb.RemoveAt(0);
+        anim = GetComponent<Animator>();
     }
 
-    public void SetRagdoll()
+    public void Ragdoll(InputAction.CallbackContext context)
     {
-        
+        if (context.performed)
+        {
+            SetRagdoll(!isRagdoll);
+        }
+
     }
 
     public void Dash()
     {
-        
+
     }
 
-    public void Move()
+    public void Move(InputAction.CallbackContext context)
     {
+        Vector3 movementInput = context.ReadValue<Vector2>();
+        Vector3 movement = new Vector3(movementInput.x, 0f, movementInput.y);
+
+        if (!isRagdoll)
+            rb.velocity = movement * walkSpeed;
+        else
+            rb.velocity = movement * ragdollSpeed;
+
+
         
+       
+        //isMoving = rb.velocity.magnitude > 0.2f;
+
     }
 
+    public void SetRagdoll(bool value)
+    {
+        anim.enabled = !value;
+        isRagdoll = value;
+        rb.isKinematic = value;
+        foreach(Rigidbody rb in ragdollRb)
+        {
+            rb.isKinematic = !value;
+        }
+    }
 }
