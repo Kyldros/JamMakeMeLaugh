1a158f655fff9c10dea4484b707dc77e7b127f73
xGiordan23x <francigio23@gmail.com>
2024-01-25 18:43:56 +0100

    robe player con anim

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,11 +11,20 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float walkSpeed;
     [SerializeField] private float ragdollSpeed;
+    [SerializeField] private float sprintSpeed;
+    public GameObject mesh;
+    public GameObject boneToMove;
     public Rigidbody rb;
-    public List<Rigidbody> ragdollRb;
+    public Collider coll;
+    private List<Rigidbody> ragdollRb;
+    private List<Collider> ragdollColl;
 
+    private Vector2 moveDirection;
+    private Vector3 lastMovement;
     private bool isRagdoll = false;
+    private bool isMoving;
     private Animator anim;
+    private bool isSprinting;
 
     public int maxHp = 5;
     public int currentHP = 5;
@@ -29,14 +38,36 @@ public class Player : MonoBehaviour
     private void Start()
     {
         currentHP = maxHp;
+        foreach (Rigidbody rb in ragdollRb)
+        {
+            rb.isKinematic = true;
+        }
+        foreach (Collider col in ragdollColl)
+        {
+            col.enabled = false;
+        }
+
+
+    }
+    private void Update()
+    {
+        Move2(moveDirection);
+        if (isRagdoll)
+        {
+            boneToMove.GetComponent<Rigidbody>().MovePosition(transform.position);
+        }
+
     }
 
     public void OnEnable()
-
     {
         ragdollRb = GetComponentsInChildren<Rigidbody>().ToList();
         ragdollRb.RemoveAt(0);
-        anim = GetComponent<Animator>();
+
+        ragdollColl = GetComponentsInChildren<Collider>().ToList();
+        ragdollColl.RemoveAt(0);
+
+        anim = GetComponentInChildren<Animator>();
     }
 
     public void Ragdoll(InputAction.CallbackContext context)
@@ -55,40 +86,87 @@ public class Player : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context)
     {
-        Vector3 movementInput = context.ReadValue<Vector2>();
-        Vector3 movement = new Vector3(movementInput.x, 0f, movementInput.y);
-
-        if (!isRagdoll)
-            rb.velocity = movement * walkSpeed;
-        else
-            rb.velocity = movement * ragdollSpeed;
-
-
-
-
-        //isMoving = rb.velocity.magnitude > 0.2f;
+        moveDirection = context.ReadValue<Vector2>();
 
     }
+    public void Sprint(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            isSprinting = true;
+            anim.SetBool("isSprinting", isSprinting);
+        }
+        if(context.canceled)
+        {
+            isSprinting = false;
+            anim.SetBool("isSprinting", isSprinting);
+        }
+    }
+    public void Move2(Vector2 moveDirection)
+    {
+
+        Vector3 movement = new Vector3(moveDirection.x, 0, moveDirection.y);
 
 
+        if (isRagdoll && !isSprinting)
+        {
+            rb.velocity = new Vector3(moveDirection.x * ragdollSpeed, rb.velocity.y, moveDirection.y * ragdollSpeed);
+        }
+
+        else
+        {
+            rb.velocity = new Vector3(moveDirection.x * walkSpeed, rb.velocity.y, moveDirection.y * walkSpeed);
+
+                if (isSprinting)
+                rb.velocity = new Vector3(moveDirection.x * sprintSpeed, rb.velocity.y, moveDirection.y * sprintSpeed);
+
+        }
+
+
+        if (lastMovement != movement && !isRagdoll)
+        {
+            lastMovement = movement;
+
+            if (movement == Vector3.right)
+            {
+                mesh.transform.rotation = Quaternion.Euler(new Vector3(0, 90, 0));
+            }
+            else if (movement == Vector3.left)
+            {
+                mesh.transform.rotation = Quaternion.Euler(new Vector3(0, -90, 0));
+            }
+        }
+
+        isMoving = rb.velocity.magnitude > 0.2f;
+        anim.SetBool("isMoving", isMoving);
+
+
+    }
     public void SetRagdoll(bool value)
     {
         anim.enabled = !value;
         isRagdoll = value;
-        rb.isKinematic = value;
+        rb.velocity = Vector3.zero;
+
+
         foreach (Rigidbody rb in ragdollRb)
         {
             rb.isKinematic = !value;
         }
-    }
-        public void takeDamage(int damage)
+        foreach (Collider col in ragdollColl)
         {
-            currentHP -= damage;
-            if (currentHP <= 0)
-                onDeath?.Invoke();
-            else
-                onDamage?.Invoke();
-
+            col.enabled = value;
         }
     }
+
+    public void takeDamage(int damage)
+    {
+        currentHP -= damage;
+        if (currentHP <= 0)
+            onDeath?.Invoke();
+        else
+            onDamage?.Invoke();
+
+    }
+}
 
