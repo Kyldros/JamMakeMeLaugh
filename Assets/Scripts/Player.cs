090c69481d99fb4b78a8f1772073496fc925cf53
xGiordan23x <francigio23@gmail.com>
2024-01-25 12:51:28 +0100

    afafe

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] PlayerInputSystem inputActions;
+
+    private bool isRagdoll;
+    
+
+    
+    public    void OnEnable()
+    {
+       
+    }
+
+    public void SetRagdoll()
+    {
+        
+    }
+
+    public void Dash()
+    {
+        
+    }
+
+    public void Move()
+    {
+        
+    }
+
+}
