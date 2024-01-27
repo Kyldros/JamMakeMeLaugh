5d366120aeee02dd40e42345dc9744e29636c8d1
Kyldros <kyldros@gmail.com>
2024-01-27 11:52:12 +0100

    pu

---
+++ Assets/Scripts/PlatformerEffector3D/PlatformerEffector3D.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerEffector3D : MonoBehaviour
+{
+    public Collider platformColliderToDisable;
+
+    public void DisableCollider()
+    {
+        platformColliderToDisable.enabled = false;
+    }
+
+    public void EnableCollider()
+    {
+        platformColliderToDisable.enabled = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Vector3 direction = other.transform.position - transform.position;
+
+        if (direction.y < 0)
+        {
+            DisableCollider();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        EnableCollider();
+    }
+
+}
+
+
